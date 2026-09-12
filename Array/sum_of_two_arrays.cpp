2547f0728959518de7f8ff5bbc05a6bc17a31c41
Snehal Narale <snehalnarale05@gmail.com>
2026-09-12 16:17:54 +0530

    Sum of two arrays

---
+++ Array/sum_of_two_arrays.cpp
@@ -0,0 +1,67 @@
+vector<int> reverse(vector<int> v)
+{
+    int s = 0;
+    int e = v.size() - 1;
+
+    while (s < e)
+    {
+        swap(v[s++], v[e--]);
+    }
+    return v;
+}
+
+#include <bits/stdc++.h>
+vector<int> findArraySum(vector<int> &a, int n, vector<int> &b, int m)
+{
+    // Write your code here.
+    int i = n - 1;
+    int j = m - 1;
+    int carry = 0;
+
+    vector<int> ans;
+
+    while (i >= 0 && j >= 0)
+    {
+        int val1 = a[i];
+        int val2 = b[j];
+
+        int sum = val1 + val2 + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+    // 1st case    e.g 1 2 3 4
+    //       6
+    while (i >= 0)
+    {
+        int sum = a[i] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+
+        i--;
+    }
+    // 2nd case     e.g       6
+    //   1 2 3 4
+    while (j >= 0)
+    {
+        int sum = b[j] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+
+        j--;
+    }
+    // 3rd case    e.g   9 9 9
+    //     9 9 9
+    while (carry != 0)
+    {
+        int sum = carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+    }
+    return reverse(ans);
+}
